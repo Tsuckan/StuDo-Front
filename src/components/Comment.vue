cf896b6d28811d04f13aa00a54424c371192e55e
Tsuckan <tsuckanovdmitriy@gmail.com>
2020-04-03 23:02:17 +0300

    Added Favorited and Comments system

---
+++ src/components/Comment.vue
@@ -0,0 +1,253 @@
+
+<template>
+    <div class="main">
+        <header>
+            <div class="logoBlock d-flex">
+                <div class="logo d-flex">
+                    <div class="imgLogo">
+                        <img src="../../src/assets/logo.png" height="50px" width="50px"/></div>
+                    <div class="nameLogo">
+                        StuDo
+                    </div>
+                </div>
+
+
+
+            </div>
+        </header>
+        <div class="blur_test">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div class="menuBar">
+                            <div class="btnsMenu">
+                                <router-link style="position: relative" class="menuBarBut" to="/Create">Создать объявление</router-link>
+                                <br>
+                                <router-link style="position: relative" class="menuBarBut" to="/ResumeCreate">Создать Резюме</router-link>
+                                <div class="btnMenuItems d-flex">
+                                    <div class="btnActiv"></div>
+                                    <router-link style="position: relative" to="/Logged">Все объявления</router-link>
+                                </div>
+                                <div class="btnMenuItems d-flex">
+                                    <div class="btnPassiv"></div>
+                                    <router-link style="position: relative"  to="/MyLogged">Мои объявления</router-link>
+                                </div>
+                                <div class="btnMenuItems d-flex">
+                                    <div class="btnPassiv"></div>
+                                    <div class="textBtns">Закладки</div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="postBlocks">
+
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="topMenu d-flex">
+                            <div class="topMenuItems active">
+
+                                <router-link  to="/Logged">Объявления</router-link>
+                            </div>
+                            <div class="topMenuItems">
+                                <router-link  to="/Resumes">Резюме</router-link>
+                            </div>
+                            <div class="topMenuItems">
+                                <router-link  to="/Profile">Профиль</router-link>
+                            </div>
+                        </div>
+
+                        <div class="rightBlock">
+                            <div class="rightBlock_firstBlock">
+                                <div class="searchform d-flex">
+                                    <input type="text" class="searchInput">
+                                    <div class="rightBlock_firstBlock_searchLogo">
+                                        <i class="fa fa-search" aria-hidden="true"></i>
+                                    </div>
+                                </div>
+                                <div class="sortBlock">
+                                    Сортировка
+                                    <div class="sortItems">
+                                        <div class="sortItem d-flex">
+                                            <div class="sortItemsStatus sortItemsStatusActiv"></div>По дате создания
+                                        </div>
+                                        <div class="sortItem d-flex">
+                                            <div class="sortItemsStatus"></div>Категории
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="Auth">
+            <div>
+                <h1>Смена пароля</h1>
+                <label for="description">Текст Комментария</label><br>
+                <textarea cols="103" required placeholder="" id="description" v-model="description" name="description" type="text"></textarea> <br>
+                <router-link class="Registerbtn" to="/Logged">Назад</router-link>
+
+                    <button class="Login_BTN" @click="handleSubmit">
+                        Отправить
+                    </button>
+                </div>
+            </div>
+        </div>
+</template>
+<script>
+    import router from "@/router";
+    import axios from 'axios';
+    export default {
+        name: "Comment",
+        props: ['id'],
+        data(){
+            return {
+                postid: this.$router.currentRoute.params['id']
+            }
+        },
+        mounted()
+        {
+        },
+        methods : {
+            handleSubmit(e){
+                e.preventDefault()
+                axios({
+                    headers: {
+                        'Authorization': "bearer " + this.$cookies.get("ACCESSTOKEN")
+                    },
+                    method: 'post',
+                    url: 'https://dev.studo.rtuitlab.ru/api/ad/comment/'+this.postid,
+                    data: {
+                        text: this.description
+                    }
+                })
+                    // eslint-disable-next-line no-unused-vars
+                    .then(({ data }) => {
+                        // eslint-disable-next-line no-console
+                        console.log('status: ', data.status);
+                        router.push("/Logged");
+                    }).catch(error => {
+                    if(error)
+                        // eslint-disable-next-line no-console
+                        console.log('status: ', error.code);
+                    this.$popup('append', 'Произошла ошибка');
+                });
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .blur_test
+    {
+        filter: blur(20px);
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: 100vh;
+        background-size: 100%;
+        margin: 0;
+    }
+    .Auth
+    {
+        display: flex;
+        justify-content: center; /*Центрирование по горизонтали*/
+        align-items: center;
+    }
+    .blur_test, .Auth
+    {
+
+        position: absolute;
+        width: 100%;
+        height: 100%    ;
+        left: 0;
+        top: 0;
+        bottom: 0;
+        right: 0;
+    }
+    .Auth > div h1
+    {
+        color: rgb(255,254,255);
+        padding-left: 30px;
+        padding-top: 30px;
+        padding-bottom: 10px;
+        background-color: rgb(36,35,37);
+        border-radius: 15px;
+    }
+    .Auth > div label
+    {
+        color: rgb(85,84,85);
+        margin-left: 30px;
+    }
+    .Auth > div label
+    {
+        margin-top: 30px;
+        color: rgb(85,84,85);
+        margin-left: 30px;
+    }
+
+    textarea
+    {
+        width: 480px;
+        background-color: rgb(54,55,54);
+        border: none;
+        border-radius: 15px;
+        height: 35px;
+        resize: none;
+        margin-left: 30px;
+    }
+    .Auth > div input
+    {
+        width: 480px;
+        background-color: rgb(54,55,54);
+        border: none;
+        border-radius: 15px;
+        height: 35px;
+        margin-left: 30px;
+    }
+    .Auth > div
+    {
+        border-radius: 15px;
+        width: 540px;
+        height: 410px;
+        background-color: rgb(46,45,46);
+    }
+    .buttons
+    {
+        height: 93px;
+        background-color: rgb(36,35,37);
+        margin-top: 26px;
+        border-radius: 15px;
+    }
+    .Login_BTN
+    {
+        float: right;
+        border-radius: 30px;
+        background-color: rgb(102,49,179);
+        border: none;
+        color: white;
+        width: 120px;
+        margin-top: 20px;
+        height: 46px;
+    }
+    .Registerbtn
+    {
+        display: inline-block;
+        margin-left: 30px;
+        margin-top: 15px;
+        color: white;
+    }
+    .Forgoten
+    {
+        position: relative;
+        bottom: -40px;
+        left: -95px;
+        color: white;
+    }
+</style>
---
+++ src/components/Favorited.vue
@@ -0,0 +1,423 @@
+/* eslint-disable */
+<template>
+    <div class="box">
+        <VuePopupPlugin :config="popupDefaultConfig"/>
+        <header>
+            <div class="logoBlock d-flex">
+                <div class="logo d-flex">
+                    <div class="imgLogo">
+                        <img src="../../src/assets/logo.png" height="50px" width="50px"/></div>
+                    <div class="nameLogo">
+                        StuDo
+                    </div>
+                </div>
+
+
+
+            </div>
+        </header>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="menuBar">
+                        <div class="btnsMenu">
+                            <router-link style="position: relative; color: white;" class="menuBarBut" to="/Create">Создать объявление</router-link>
+                            <router-link style="position: relative; color: white;" class="menuBarBut" to="/ResumeCreate">Создать Резюме</router-link>
+                            <div class="btnMenuItems d-flex">
+                                <div class="btnPassiv"></div>
+                                <router-link style="position: relative; color: white;" to="/Logged">Все объявления</router-link>
+                            </div>
+                            <div class="btnMenuItems d-flex">
+                                <div class="btnPassiv"></div>
+                                <router-link style="position: relative; color: white;"  to="/MyLogged">Мои объявления</router-link>
+                            </div>
+                            <div class="btnMenuItems d-flex">
+                                <div class="btnActiv"></div>
+                                <router-link style="position: relative; color: white;"  to="/Favorited">Закладки</router-link>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="postBlocks" v-for="post in posts" :key="post.id">
+                        <div class="postBlock">
+                            <div class="postTopBlock">
+                                <button class="BookmarkBtn" @click="Bookmark(post.id)">
+                                </button>
+                                <div class="blockTopForLogo">
+                                    <i class="fa fa-ambulance" aria-hidden="true"></i>
+                                </div>
+                                <div class="titleForPost"><router-link :to="{name: 'Ad', params: {id: post.id}, props: {id: post.id}}"
+                                >{{post.name}}</router-link>
+                                </div>
+                            </div>
+                            <div class="postDownBlock">
+                                <div class="textblockForPost">
+                                    {{post.shortDescription}}
+                                </div>
+                                <div class="postdate">
+                                    {{formatDate(new Date(post.beginTime))}} - {{formatDate(new Date(post.endTime))}}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="topMenu d-flex">
+                    <div class="topMenuItems active">
+
+                        <router-link style="position: relative; color: white;" to="/Logged">Объявления</router-link>
+                    </div>
+                    <div class="topMenuItems">
+                        <router-link style="position: relative; color: white;" to="/Resumes">Резюме</router-link>
+                    </div>
+                    <div class="topMenuItems">
+                        <router-link style="position: relative; color: white;" to="/Profile">Профиль</router-link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import router from "@/router";
+    import axios from 'axios';
+    export default {
+        name: "Logged",
+        data() {
+            return {
+                rawHtml: {},
+                posts: []
+            };
+        },methods : {
+            formatDate(date) {
+                var dd = date.getDate();
+                if (dd < 10) dd = '0' + dd;
+                var mm = date.getMonth() + 1;
+                if (mm < 10) mm = '0' + mm;
+                var yy = date.getFullYear();
+                if (yy < 10) yy = '0' + yy;
+                return dd + '.' + mm + '.' + yy;
+            },
+            Bookmark(a) {
+                this.$popup('append', 'Пост удалён из закладок');
+                axios({
+                    headers: {
+                        'Authorization': "bearer " + this.$cookies.get("ACCESSTOKEN")
+                    },
+                    method: 'delete',
+                    url: 'https://dev.studo.rtuitlab.ru/api/ad/bookmarks/' + a,
+                    data: {}
+                })
+                window.location.reload();
+            },
+            handleSubmit(Idval) {
+                router.push({path: '/Ad', query: {Id: Idval}})
+            },
+            Create() {
+                router.push("/Resumes")
+            },
+        },
+        mounted() {
+            axios({
+                headers: {
+                    'Authorization': "bearer " + this.$cookies.get("ACCESSTOKEN")
+                },
+                method: 'get',
+                url: 'https://dev.studo.rtuitlab.ru/api/ad/bookmarks',
+                data: {}
+            })
+                .then(data => {
+                    this.posts=data.data;
+                    // eslint-disable-next-line no-console
+                    console.log(data)
+                }).catch(error => {
+                if(error)
+                    router.push("/Login");
+            });
+        }}
+</script>
+
+<style scoped>
+    .qq{
+        border: 1px solid black;
+    }
+    .BookmarkBtn
+    {
+        float: right;
+        padding-bottom: 20px;
+        margin-right: 10px;
+    }
+
+    header{
+        height: 50px;
+        background: #222222;
+        margin: 0 auto;
+    }
+    body{
+        background: #141414;
+        font-family: Roboto;
+
+
+    }
+    .logoBlock{
+        width: 213px;
+        background: #2F2F2F;
+        height: 70px;
+        border-bottom-left-radius: 13px;
+        border-bottom-right-radius: 13px;
+        margin: 0 auto;
+        position: relative;
+        z-index: 1;
+    }
+    .imgLogo{
+
+    }
+    .nameLogo{
+        font-size: 24px;
+        color: white;
+        margin-top: 10px;
+        margin-left: 10px;
+
+    }
+    .logo{
+        margin: 5px auto;
+    }
+    .menuBar{
+        width: 319px;
+
+        position: fixed;
+        margin-left: 20px;
+
+    }
+    .menuBarBut{
+        width: 319px;
+
+        height: 51px;
+        border-radius: 13px;
+        border-bottom: 3px solid #673AB7;
+        background: #2F2F2F;
+        margin-top: 37px;
+        color: white;
+        font-size: 18px;
+        text-align: center;
+        padding-top: 10px;
+
+
+    }
+    .btnMenuItems{
+        margin-top: 37px;
+    }
+    .btnMenuItems{
+        width: 319px;
+
+        height: 40px;
+        border-radius: 8px;
+        background: #222222;
+        margin-top: 5px;
+        color: white;
+        font-size: 18px;
+
+        padding-left: 20px;
+    }
+    .btnsMenu{
+        margin-top: 37px;
+
+
+    }
+    .btnActiv{
+
+        width: 6px;
+        height: 40px;
+        background: #673AB7;
+        margin-right: 20px;
+
+
+    }
+    .btnPassiv{
+        width: 6px;
+        height: 40px;
+        background: #2F2F2F;
+        margin-right: 20px;
+
+
+    }
+    .textBtns{
+        padding-top: 7px;
+    }
+    .topMenu{
+        width: 319px;
+        height: 46px;
+        background: #2F2F2F;
+        border-radius: 13px;
+        position: fixed;
+        top:25px;
+        display: flex;
+        margin-left: 40px;
+        justify-content: space-around;
+        text-transform: uppercase;
+    }
+    .topMenuItems{
+        color: white;
+        font-size: 14px;
+        margin-top: 12px;
+
+
+    }
+    .active{
+        border-bottom: 2px solid white;
+    }
+    .postBlock{
+        width: 100%;
+        margin: 0 auto;
+        height: auto;
+        margin-top: 50px;
+        background: #3B3B3B;
+        padding-left: 0px;
+        border-radius: 13px;
+    }
+    .postTopBlock{
+        height: 63px;
+    }
+    .postDownBlock{
+        position: relative;
+        width: 100%;
+        height: auto;
+        padding-bottom: 15px;
+        background: #2F2F2F;
+        border-radius: 0 0 13px 13px;
+    }
+    .titleForPost{
+        color: white;
+        font-size: 19px;
+        margin: 0 auto;
+        padding-top: 15px;
+        padding-left: 90px;
+    }
+    .blockTopForLogo{
+        width: 48px;
+        height: 53px;
+        border-radius: 5px 5px 13px 13px;
+        background: #673AB7;
+        position: absolute;
+        z-index: 1;
+        margin-top: -5px;
+        margin-left: 25px;
+        color: white;
+        font-size: 25px;
+        text-align: center;
+        padding-top: 10px;
+    }
+    .textblockForPost{
+        width: 90%;
+        height: auto;
+        margin-left: 25px;
+        padding-top: 19px;
+        font-size: 16px;
+        color: #ACACAC;
+    }
+    .postBlocks{
+        padding-top: 35px;
+        padding-bottom: 47px;
+
+    }
+    .rightBlock_firstBlock{
+        padding-top: 15px;
+        height: 204px;
+        background: #222222;
+        border-radius: 13px;
+
+
+    }
+    .rightBlock{
+        width: 319px;
+        margin-top: 59px;
+        position: fixed;
+
+        margin-left: 40px;
+    }
+    .rightBlock_secondBlock{
+        margin-top: 39.2px;
+        height: 285px;
+        background: #222222;
+        border-radius: 13px;
+        padding-top: 1px;
+    }
+    .searchform{
+        background: #3B3B3B;
+        border-radius: 36px;
+        color: white;
+        border: 1px solid #3B3B3B;
+        width: 279px;
+        margin-left: 19px;
+        padding-top: 1px;
+
+
+    }
+    .searchInput{
+        background: #3B3B3B;
+        border-radius: 36px 0 0 36px;
+        color: white;
+        border: 1px solid #3B3B3B;
+        width: 245px;
+        margin-right: 5px;
+        padding-left: 5px;
+
+
+    }
+    .sortBlock{
+        width: 290px;
+        height: 100px;
+
+        margin-left: 19px;
+        margin-top: 27.4px;
+        color: #ACACAC;
+        font-size: 16px;
+    }
+    .catBlock{
+        width: 290px;
+
+
+        margin-left: 19px;
+        margin-top: 14px;
+        color: #ACACAC;
+        font-size: 16px;
+    }
+    .sortItems{
+        color: white;
+    }
+    .sortItemsStatus {
+        margin-top: 6px;
+        margin-right: 14px;
+        background: #3B3B3B;
+        border-radius: 50%;
+        height: 10px;
+        width: 10px;
+    }
+    .sortItem{
+        margin-top: 15px;
+
+    }
+    .sortItemsStatusActiv{
+
+        background:  #673AB7;
+
+    }
+    .catBlock .sortItemsStatus{
+        border-radius: 0;
+    }
+    .countCat{
+        text-align: right;
+        position: absolute;
+        right: 22px;
+        font-size: 18px;
+        color: #ACACAC;
+    }
+    .postdate
+    {
+        margin-left: 40%;
+    }
+</style>
