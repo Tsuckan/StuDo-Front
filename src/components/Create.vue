ab10fff4eac069c4b8494b3bd2ba4a8da6d972b1
ivlaptev <ivlaptev13@ya.ru>
2020-05-04 23:25:13 +0300

    Fix request URL

--- src/components/Create.vue
+++ src/components/Create.vue
@@ -147,7 +147,7 @@
                         'Authorization': "bearer " + this.$cookies.get("ACCESSTOKEN")
                     },
                     method: 'post',
-                    url: process.env.VUE_APP_API + 'api/ad',
+                    url: process.env.VUE_APP_API + 'ad',
                     data: {
                         "name": this.name,
                         "description": this.description,
