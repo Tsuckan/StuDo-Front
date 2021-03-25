283d7f811d36f87ddd7e9ca0fd1f5a95afbc774b
Nick <nikitamm59rus@gmail.com>
2021-03-25 19:52:35 +0300

    nf fix

--- src/components/nf.vue
+++ src/components/nf.vue
@@ -5,7 +5,7 @@
             var s = decodeURIComponent(this.$route.fullPath);
             // eslint-disable-next-line no-console
             console.log(s);
-            window.location.href = process.env.VUE_APP_FRONT + 'ads';
+            window.location.href = process.env.VUE_APP_FRONT + '/ads';
         }
     }
 </script>
